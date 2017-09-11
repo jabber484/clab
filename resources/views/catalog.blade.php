094aa3ad2e6199842904074e94a2bfc5c505ab42
Johnson Ma <jabber484@gmail.com>
2017-09-11 11:56:04 +0800

    added search function

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -12,14 +12,25 @@
 		</div>
 		<div class="col-xs-12">
 			<input class="catalog-search-bar" type="text" name="search" placeholder="Search">
+			@foreach($payload as $type => $records)
+			<div class="form-check">
+				<label class="form-check-label">
+					<input class="form-check-input search-checkbox" type="checkbox" name="search" value="{{str_replace(" ","-",strtolower($type))}}">
+					{{$type}}
+				</label>
+			</div>
+			@endforeach
+		</div>
+		<div class="catalog-empty col-xs-12" style="display:none;">
+			No Result
 		</div>
 		@foreach($payload as $type => $records)
-		<div id="{{str_replace(" ","-",$type)}}" class="col-xs-12">
+		<div class="col-xs-12 catalog-subtitle-wrapper" id="{{str_replace(" ","-",strtolower($type))}}">
 			<div class="catalog-subtitle">{{$type}}</div>
 			<div class="row">
 				@foreach($records as $record)
-				<div class="col-xs-12 col-sm-6 col-lg-4">
-					<div class="item" id="{{$record['id']}}">
+				<div class="col-xs-12 col-sm-6 col-lg-4 item-wrapper" id="{{$record['id']}}">
+					<div class="item">
 						<div class="item-img">	
 							<img src="{{asset('asset/catalogue/'.$record['id'].'.jpg')}}">
 						</div>	
@@ -42,21 +53,52 @@
 var data = JSON.parse('{!!json_encode($payload)!!}');
 console.log(data);
 
-function hideAllitam(){
-	
+function hideAllItems(){
+	$(".item-wrapper").css("display","none");
+}
+
+function hideAllTitles(){
+	$(".catalog-subtitle-wrapper").css("display","none");
+}
+
+function showTitle(id){
+	$(".catalog-subtitle-wrapper#"+id).css("display","block");
+}
+
+function showItem(id){
+	$(".item-wrapper#"+id).css("display","block");
+}
+
+function showSubsection(id){
+	showTitle(id);
+	$(".catalog-subtitle-wrapper#"+id+" .item-wrapper").css("display","block");
+}
+
+function hideEmtpyMsg(){
+	$(".catalog-empty").css("display","none");
+}
+
+function showEmtpyMsg(){
+	$(".catalog-empty").css("display","block");
 }
 
 //search
 $('.catalog-search-bar').keyup(function(event) {
 	if ( event.which == 13 )
 	   event.preventDefault();
-	
+
 	var seed = $('.catalog-search-bar').val().toLowerCase();
 	var result = {};
 	var isType = false;
+	var lengthSum = 0;
 
+	hideEmtpyMsg();
+	hideAllItems();
+	hideAllTitles();
+
+	//match
 	$.each(data, function(type,records){
-		type = type.replace(" ","-");
+		type = type.toLowerCase().replace(" ","-");
 		result[type] = [];
 
 		if(type.toLowerCase() == seed){
@@ -70,11 +112,44 @@ $('.catalog-search-bar').keyup(function(event) {
 		}
 	});
 
+	//render
 	if(isType){
-		// $('')
+		showSubsection(seed.replace(" ","-"));
 	} else {		
-		console.log(result);
+		$.each(result, function(id,records){
+			if(records.length > 0){
+				lengthSum++;
+
+				showTitle(id);
+				$.each(records, function(index,itemId){
+					showItem(itemId);
+				});
+			}
+		});
+
+		if(lengthSum == 0)
+			showEmtpyMsg();
 	}
 });
+
+//checkbox
+$('input').click(function(event) {
+	var input = $('.search-checkbox:checked');
+	console.log(input);
+	if(input.length == 0){
+		$(".item-wrapper").css("display","block");
+		$(".catalog-subtitle-wrapper").css("display","block");
+
+		return;
+	}
+
+	hideAllItems();
+	hideAllTitles();
+
+	input.each(function(index) {
+		showSubsection($(this).val());	
+	});
+});
+
 </script>
 @endsection
