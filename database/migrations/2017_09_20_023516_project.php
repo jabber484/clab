916d3e1f324a390d40eb71b1c08cc08ca0963ea4
Johnson Ma <jabber484@gmail.com>
2017-09-20 10:56:29 +0800

    minor

---
+++ database/migrations/2017_09_20_023516_project.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Project extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->increments('id')->unique();
+            $table->enum('type',['Art and Culture', "Design Thinking", "Entrepreneurship and Management", "Science and Technology", "Sociopolitical Issues"]);
+            $table->string('name');
+            $table->text('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('catalogs');
+    }
+}
